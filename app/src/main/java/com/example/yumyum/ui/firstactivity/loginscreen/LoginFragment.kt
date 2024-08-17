136ded684ac1912bb92128f871227f0638741ae4
Uosef <uoseftalaat0@gmail.com>
2024-08-17 22:23:20 +0300

    add color files

--- app/src/main/java/com/example/yumyum/ui/firstactivity/loginscreen/LoginFragment.kt
+++ app/src/main/java/com/example/yumyum/ui/firstactivity/loginscreen/LoginFragment.kt
@@ -16,7 +16,7 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentLoginBinding.inflate(layoutInflater,container,false)
         return binding.root
     }
